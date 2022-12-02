98993ab4d8ccbd020f6da9b45daafe05075f9e99
Cark <carkhy@gmail.com>
2022-12-02 11:06:05 +0100

    simplified

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,31 +1,33 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    let data = parse(INPUT);
-    println!("Part1 : {}", part1(&data));
-    println!("Part2 : {}", part2(&data));
+    let setup_time = std::time::Instant::now();
+    let part1 = part1(INPUT);
+    let part1_dur = setup_time.elapsed().as_nanos();
+    let setup_time = std::time::Instant::now();
+    let part2 = part2(INPUT);
+    let part2_dur = setup_time.elapsed().as_nanos();
+    println!("Part1: {part1} in {part1_dur} ns");
+    println!("Part2: {part2} in {part2_dur} ns");
 }
 
-fn part1(data: &[u8]) -> u64 {
+fn part1(data: &str) -> u64 {
     solve(data, 1)
 }
 
-fn part2(data: &[u8]) -> u64 {
+fn part2(data: &str) -> u64 {
     solve(data, data.len() / 2)
 }
 
-fn solve(data: &[u8], offset: usize) -> u64 {
+fn solve(data: &str, offset: usize) -> u64 {
+    let data = data.as_bytes();
     let mut result = 0;
     for (i, &value) in data.iter().enumerate() {
         if value == data[(i + offset) % data.len()] {
-            result += value as u64
+            result += (value - b'0') as u64
         }
     }
     result
-}
-
-fn parse(input: &str) -> Vec<u8> {
-    input.trim().as_bytes().iter().map(|b| b - b'0').collect()
 }
 
 #[cfg(test)]
@@ -34,18 +36,18 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(part1(&parse("1122")), 3);
-        assert_eq!(part1(&parse("1111")), 4);
-        assert_eq!(part1(&parse("1234")), 0);
-        assert_eq!(part1(&parse("91212129")), 9);
+        assert_eq!(part1("1122"), 3);
+        assert_eq!(part1("1111"), 4);
+        assert_eq!(part1("1234"), 0);
+        assert_eq!(part1("91212129"), 9);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(part2(&parse("1212")), 6);
-        assert_eq!(part2(&parse("1221")), 0);
-        assert_eq!(part2(&parse("123425")), 4);
-        assert_eq!(part2(&parse("123123")), 12);
-        assert_eq!(part2(&parse("12131415")), 4);
+        assert_eq!(part2("1212"), 6);
+        assert_eq!(part2("1221"), 0);
+        assert_eq!(part2("123425"), 4);
+        assert_eq!(part2("123123"), 12);
+        assert_eq!(part2("12131415"), 4);
     }
 }
