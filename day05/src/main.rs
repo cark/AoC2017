0f5d5b1f01a3b0b9d6ee7ca0a50361c89f65b190
Cark <carkhy@gmail.com>
2022-12-03 23:40:17 +0100

    day05

---
+++ day05/src/main.rs
@@ -0,0 +1,60 @@
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let setup_time = std::time::Instant::now();
+    let part1 = part1(INPUT);
+    let part1_dur = setup_time.elapsed().as_micros();
+    println!("Part1 : {} in {} µs", part1, part1_dur);
+
+    let setup_time = std::time::Instant::now();
+    let part2 = part2(INPUT);
+    let part2_dur = setup_time.elapsed().as_micros();
+    println!("Part2 : {} in {} µs", part2, part2_dur);
+}
+
+fn part1(input: &str) -> i32 {
+    run(load_program(input), |i| i + 1)
+}
+
+fn part2(input: &str) -> i32 {
+    run(load_program(input), |i| if i >= 3 { i - 1 } else { i + 1 })
+}
+
+fn run(mut program: Vec<i32>, update_instruction: impl Fn(i32) -> i32) -> i32 {
+    let mut pi: i32 = 0;
+    let mut instruction_count = 0;
+    while pi >= 0 && pi < program.len() as i32 {
+        let index = pi as usize;
+        let offset = program[index];
+        program[index] = update_instruction(program[index]);
+        pi = pi + offset;
+        instruction_count += 1;
+    }
+    instruction_count
+}
+
+fn load_program(input: &str) -> Vec<i32> {
+    input
+        .trim()
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("test_input.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT), 5);
+        assert_eq!(part1(INPUT), 388611);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 10);
+        assert_eq!(part2(INPUT), 27763113);
+    }
+}
